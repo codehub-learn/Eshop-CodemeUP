b8e521bd46351775a2e4224e7d0d8f39f1c84a74
agag <agag19642006@gmail.com>
2022-11-10 20:33:33 +0200

    Made some changes

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -1,19 +1,23 @@
 package model;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Order {
     private long id;
     private Customer customer;
-    private List<Product> products;
+    private ArrayList<Product> products;
     private Date dateCreated;
+    private int productCount;
+    private double totalCost;
 
-    public Order(long id, Customer customer, List<Product> products, Date dateCreated) {
+    public Order(long id, Customer customer) {
         this.id = id;
         this.customer = customer;
-        this.products = products;
-        this.dateCreated = dateCreated;
+        this.products = new ArrayList<>();
+        this.dateCreated = new Date();
     }
 
     public long getId() {
@@ -36,25 +40,32 @@ public class Order {
         return products;
     }
 
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
-
     public Date getDateCreated() {
         return dateCreated;
     }
 
-    public void setDateCreated(Date dateCreated) {
-        this.dateCreated = dateCreated;
+    public void addProduct(Product product){
+        products.add(product);
+        productCount++;
+        totalCost += product.getPrice();
+    }
+
+    public void removeProduct(Product product){
+        products.remove(product);
+        productCount--;
+        totalCost -= product.getPrice();
     }
 
     @Override
     public String toString() {
+        DecimalFormat df = new DecimalFormat("0.00");
         return "Order{" +
                 "id=" + id +
                 ", customer=" + customer +
                 ", products=" + products +
                 ", dateCreated=" + dateCreated +
+                ", productCount=" + productCount +
+                ", totalCost=" + df.format(totalCost) +
                 '}';
     }
 }
