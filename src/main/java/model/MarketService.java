50542ba5267c41aed2d7a39336718b96137c13f1
agag <agag19642006@gmail.com>
2022-11-17 20:28:14 +0200

    Made some changes

--- src/main/java/model/MarketService.java
+++ src/main/java/model/MarketService.java
@@ -1,39 +1,52 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class MarketService {
-    private Product[] products;
-    private Customer customer1;
-    private Order order1;
+    private List<Product> productsInShop = new ArrayList<>();
+    private List<Customer> customers = new ArrayList<>();
+    private List<Order> orders = new ArrayList<>();
+    private Customer currentCustomer;
+    private Order currentOrder;
 
     public void initializeData() {
-        products = new Product[]{
+        productsInShop = Arrays.asList( new Product[]{
                 new Product(987654343521l, "Samsung Galaxy s22", 799.89345, Category.PHONES),
                 new Product(98423423654321l, "Iphone 11 Plus", 899.89543, Category.PHONES),
                 new Product(953453454321l, "Samsung Galaxy s21", 699.89345, Category.PHONES),
                 new Product(987642342321l, "Samsung Galaxy s20", 599.89345, Category.PHONES),
                 new Product(9874324465321l, "Samsung Galaxy s10", 499.89345, Category.PHONES),
-                new Product(987678645631l, "Headphones2", 199.89345, Category.SOUND),
-        };
-        customer1 = new Customer(123456789, "Paul Croft", "test address", "paulcroft@gmail.com");
-        order1 = new Order(234567890, customer1);
+                new Product(987678645631l, "Headphones2", 199.89345, Category.SOUND)
+        });
+        customers = Arrays.asList(new Customer[]{new Customer(123456789, "Paul Croft", "test address", "paulcroft@gmail.com")});
+        currentCustomer = customers.get(0);
+        orders = Arrays.asList(new Order[]{ new Order(234567890, currentCustomer)});
+        currentOrder = orders.get(0);
     }
 
-    public void removeProductFromOrder(int productId) {
-        order1.removeProduct(products[productId]);
+    public void removeProductFromOrder(int productPosition) {
+        if (productPosition >= 0 && productPosition < productsInShop.size()){
+            Product product = productsInShop.get(productPosition);
+        currentOrder.removeProduct(product);}
+        else {
+            System.out.println("This product does not exist");
+        }
     }
 
-    public void addProductToOrder(int productId) {
-        if (productId >= 0 && productId < products.length) {
-            order1.addProduct(products[productId]);
+    public void addProductToOrder(int productPosition) {
+        if (productPosition >= 0 && productPosition < productsInShop.size()) {
+            Product product = productsInShop.get(productPosition);
+            currentOrder.addProduct(product);
         } else {
             System.out.println("This product does not exist");
         }
     }
 
     public void showOrder() {
-
+        System.out.println(currentOrder);
     }
 
     public void doOperations() {
@@ -50,17 +63,19 @@ public class MarketService {
                     int userAnswer = scanner.nextInt();
                     removeProductFromOrder(userAnswer);
                     break;
-                case "a":
+                case "a", "add" :
                     System.out.println("Give the product number");
                     int productid = scanner.nextInt();
                     addProductToOrder(productid);
                     break;
-                case "s":
-                    System.out.println(order1);
+                case "s", "show" :
+                    showOrder();
                     break;
-                case "x":
+                case "x", "exit" :
                     exit = true;
                     break;
+                default:
+                    System.out.println("Invalid input");
             }
             if (exit) break;
 
