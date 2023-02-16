2fd9fa6b77e3152bacb287dba8735edb9833e186
Ioannis Daniil <jondan97@gmail.com>
2023-02-17 00:38:43 +0200

    further work on encryption algorithm class

--- src/main/java/EncryptionAlgorithm.java
+++ src/main/java/EncryptionAlgorithm.java
@@ -4,12 +4,25 @@ import java.util.Arrays;
 public class EncryptionAlgorithm {
 
     public static void main(String[] args) {
-        String originalMessage = "patates"; //
-        encrypt(originalMessage);
+        String originalMessage = "0000000"; //
+        long encryptedMessage = encrypt(originalMessage);
+        System.out.println(encryptedMessage);
     }
 
+    // 332 * 2 = 664
+    // 332 * 2 = 664
 
-    private static String encrypt(String originalText) {
+    // sum:
+    // 4 + 6
+    // 6 + 4
+    // multiplication:
+    // 4 * 6 = 24 ->
+
+    private static long encrypt(String originalText) {
+        // length of original text
+        // p a t a t e s: 7
+        // -127 eos 127
+        // 55 77 22 66 99 22 66: 7
         byte[] bytes = originalText.getBytes();
         // length of message
         int length = bytes.length;
@@ -17,7 +30,27 @@ public class EncryptionAlgorithm {
         int sum = bytes[0] + bytes[bytes.length/2] + bytes[bytes.length - 1];
         // if byte array contains bytes between 48-57 or 97-126
         // (if message contains number or small letters)
-        // todo: homework: finish the encrypt method
-        return "";
+        // boolean -> +1
+        boolean numberOrSmallLetter = false;
+        int numberOrSmallLetterCount = 0;
+        for (int i = 0; i < bytes.length; i++) {
+            if(bytes[i] >= 48 && bytes[i] <= 57 || bytes[i] >= 97 && bytes[i] <= 126){
+                numberOrSmallLetterCount++;
+            }
+            if(numberOrSmallLetterCount >= bytes.length/2){
+                numberOrSmallLetter = true;
+                break;
+            }
+        }
+        long hash = length + sum;
+        if(numberOrSmallLetter){
+            hash++;
+        }
+        // convert to fixed number (fixed=standard length, i.e. 10 length)
+        if(hash < 999999999){
+          // 9999999999 ^ 2 = .... > 64bits // todo next session (homework
+            // 999999998 + 2
+        }
+        return hash;
     }
 }
